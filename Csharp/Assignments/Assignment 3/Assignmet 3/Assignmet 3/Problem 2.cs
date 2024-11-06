022db85fe3cadfc30f40cce642b26f9a448dc07a
Somasekhar Pulicherla <somasekharp@infinite.com>
2024-11-06 21:36:44 +0530

    assignment3

---
+++ Csharp/Assignments/Assignment 3/Assignmet 3/Assignmet 3/Problem 2.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmet_3
+{
+    class Student
+
+    {
+        public string Rollno;
+        public string Name;
+        public string Class;
+        public string Semester;
+        public string Branch;
+
+        // -Pass the details of student like rollno, name, class, SEM, branch in constructor
+        public Student(string roll_no, string name, string Std_class, string sem, string branch)
+        {
+            Rollno = roll_no;
+            Name = name;
+            Class = Std_class;
+            Semester = sem;
+            Branch = branch;
+        }
+        public void Studentdetails()
+        {
+            Console.WriteLine("*** Student Details ***");
+            Console.WriteLine("Name : {0}", Name);
+            Console.WriteLine("Roll no : {0}", Rollno);
+            Console.WriteLine("Class : {0}", Class);
+            Console.WriteLine("Semester : {0}", Semester);
+            Console.WriteLine("Branch : {0}", Branch);
+        }
+    }
+
+    class Marks : Student
+    {
+        public Marks(string roll_no, string name, string Std_class, string sem, string branch) : base(roll_no, name, Std_class, sem, branch)
+        {
+
+        }
+        // -For marks write a method called GetMarks() and give marks for all 5 subjects
+
+        public int[] marks = new int[5];
+        public void Getmarks()
+        {
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.Write("Enter Subject {0} marks : ", i);
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        // Write a method called displayresult, which should calculate the average marks
+        public void DisplayResult()
+        {
+            Console.WriteLine(" ");
+            int Total_marks = 0;
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Total_marks += marks[i];
+            }
+            Console.WriteLine("*** Total marks ***");
+            Console.WriteLine("Total marks : {0} ", Total_marks);
+            Console.WriteLine(" ");
+            Console.WriteLine("*** Average percentage ***");
+            float average = (Total_marks / marks.Length);
+            Console.WriteLine("Average of all marks : {0}", average);
+
+            // -If marks of any one subject is less than 35 print result as failed
+
+            // -If marks of all subject is >35,but average is < 50 then also print result as failed
+
+            foreach (int i in marks)
+            {
+                if (i > 35 && average > 50) Console.WriteLine("Pass");
+                else Console.WriteLine("Fail");
+            }
+        }
+
+        public void DisplayData()
+        {
+            Studentdetails();
+            DisplayResult();
+        }
+
+    }
+    class Inheritance
+    {
+        public static void Main()
+        {
+            Console.Write("Enter Student name : ");
+            string Name = Console.ReadLine();
+            Console.Write("Enter Student Roll No : ");
+            string Rollno = Console.ReadLine();
+            Console.Write("Enter Student Class : ");
+            string Class = Console.ReadLine();
+            Console.Write("Enter Which Semester : ");
+            string sem = Console.ReadLine();
+            Console.Write("Enter Branch : ");
+            string branch = Console.ReadLine();
+            Marks mark = new Marks(Name, Rollno, Class, sem, branch);
+            Console.WriteLine(" ");
+            Console.WriteLine("*** Get marks ***");
+            mark.Getmarks();
+            Console.WriteLine(" ");
+            mark.DisplayData();
+            Console.ReadKey();
+        }
+    }
+
+}
+    
+
---
+++ Csharp/Assignments/Assignment 3/Assignmet 3/Assignmet 3/Sales problem1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmet_3
+{
+    
+        class SalesDetails
+        {
+            public static int Sales_No;
+            public static int Product_No;
+            public static int Price;
+            public static DateTime DateofSale;
+            public static int Quantity;
+            public static int TotalAmount;
+
+            // Pass the other information like
+            // SalesNo, Productno, Price,Qty and Dateof sale through constructor
+
+            public SalesDetails(int sales_no, int product_no, int price, int qty, DateTime date_of_sale)
+            {
+                Sales_No = sales_no;
+                Product_No = product_no;
+                Price = price;
+                Quantity = qty;
+                DateofSale = date_of_sale;
+
+            }
+
+            // Create a method called Sales()
+            // that takes qty, Price details of the object and updates the TotalAmount as  Qty *Price
+
+            public void Sales(int price, int qty)
+            {
+                TotalAmount = qty * price;
+            }
+        }
+        class SalesDetails1 : SalesDetails
+        {
+            public SalesDetails1(int sales_no, int product_no, int price, int qty, DateTime date_of_sale)
+                : base(sales_no, product_no, price, qty, date_of_sale)
+            {
+
+            }
+
+            // call the show data method to display the values without an object.
+            public static void Show_Data()
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine("*** Total Bill ***");
+                Console.WriteLine("Sales No: {0}", Sales_No);
+                Console.WriteLine("Product No: {0}", Product_No);
+                Console.WriteLine("Price : {0}", Price);
+                Console.WriteLine("Quantity : {0}", Quantity);
+                Console.WriteLine("Date of sale : {0}", DateofSale);
+                Console.WriteLine("Total Amount : {0}", TotalAmount);
+            }
+            public static void Main()
+            {
+                Console.Write("Enter the Sales Number:");
+                int sale_no = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Product Number:");
+                int product_no = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Price:");
+                int price = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Quantity:");
+                int quantity = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter the Date of sale:");
+                DateTime dateofsale = Convert.ToDateTime(Console.ReadLine());
+                SalesDetails Sdetails = new SalesDetails(sale_no, product_no, price, quantity, dateofsale);
+                Sdetails.Sales(price, quantity);
+                Show_Data();
+                Console.Read();
+            }
+        }
+    }
+
+
