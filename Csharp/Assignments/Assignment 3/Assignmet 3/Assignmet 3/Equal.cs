5ca61520c37595aa80cdee0eba0a16ebcebc845e
Somasekhar Pulicherla <somasekharp@infinite.com>
2024-11-06 21:54:14 +0530

    assignments

---
+++ Csharp/Assignments/Assignment 3/Assignmet 3/Assignmet 3/Equal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmet_3
+{
+    class Equal
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter First Word");
+            string first_word = Console.ReadLine();
+            Console.WriteLine("Enter Second Word");
+            string second_word = Console.ReadLine();
+            if (first_word == second_word)
+            {
+                Console.WriteLine("First Word and Second Word Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("not equal");
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
+
---
+++ Csharp/Assignments/Assignment 3/Assignmet 3/Assignmet 3/Reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignmet_3
+{
+    class Length
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a Word");
+            string word = Console.ReadLine();
+            string reversed = "";
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                reversed = reversed + word[i];
+            }
+            Console.WriteLine(reversed);
+            Console.ReadLine();
+        }
+    }
+}
+    
+
